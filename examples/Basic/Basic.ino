f464f16cebee81c8de22ac1a9f97b38b13066525
saikat mohanta <saikatmohantabkp@gmail.com>
2023-06-26 22:49:22 +0530

    Basic.ino

--- examples/Basic/Basic.ino
+++ examples/Basic/Basic.ino
@@ -1,1 +1,23 @@
+#include <MLPLibrary.h>
+
+MLPLibrary mlp(2, 3, 1, 0.1);  // Specify the input size, hidden size, output size, and learning rate
+
+void setup() {
+  // Initialize the MLP network
+  mlp.initialize();
+}
+
+void loop() {
+  float input[2] = {0.5, 0.3};
+  float target[1] = {0.8};
+
+  // Train the MLP network with the input and target
+  mlp.train(input, target);
+
+  // Get the output from the trained network
+  float* output = mlp.getOutput();
+
+  // Do something with the output result
+  // ...
+}
 
