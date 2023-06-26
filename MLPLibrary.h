c3bacd7a25fcd4b966c625425a8fb74f3ad5cbbd
saikat mohanta <saikatmohantabkp@gmail.com>
2023-06-27 00:35:48 +0530

    MLPLibrary.h

--- MLPLibrary.h
+++ MLPLibrary.h
@@ -3,9 +3,9 @@
 
 #include "Arduino.h"
 
-#define MAX_INPUT_SIZE 30
-#define MAX_HIDDEN_SIZE 40
-#define MAX_OUTPUT_SIZE 25
+#define MAX_INPUT_SIZE 15
+#define MAX_HIDDEN_SIZE 20
+#define MAX_OUTPUT_SIZE 15
 
 class MLPLibrary {
 public:
