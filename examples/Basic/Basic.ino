12a89567ab7e3679a3c898fe45b8ff0b13aacae6
saikat mohanta <saikatmohantabkp@gmail.com>
2023-06-27 00:17:06 +0530

    Basic.ino

--- examples/Basic/Basic.ino
+++ examples/Basic/Basic.ino
@@ -1,26 +1,25 @@
 #include "MLPLibrary.h"
 
-MLPLibrary mlp(2, 3, 1, 0.1);  // Specify the input size, hidden size, output size, and learning rate
+MLPLibrary mlp(2, 4, 1, 0.1);  // Specify the input size, hidden size, output size, and learning rate
 
 void setup() {
   // Initialize the MLP network
   mlp.initialize();
+  Serial.begin(9600);
 }
 
 void loop() {
   float input[2] = {0.5, 0.3};
-  float target[1] = {0.8};
+  float target[1] = {0.7};
 
   // Train the MLP network with the input and target
   mlp.train(input, target);
 
-  // Get the output from the trained network
-  float* output = mlp.getOutput();
+  // Predict the output using the trained network
+  float output[1];
+  mlp.predict(input, output);
+  Serial.println(output[1]);
 
   // Do something with the output result
   // ...
 }
-
-//In this example, I assumed a simplified MLP architecture with a single hidden layer. You can modify the library to fit your specific requirements, 
-//such as adding more hidden layers or changing the activation function. Keep in mind the limitations of the Arduino platform and consider optimizing the 
-//implementation further if needed.
